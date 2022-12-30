366739724e97f7ca1b5c6faec2d6ce983fba7d9f
tengfei <shentengfei@axzt.net>
2022-12-30 15:12:25 +0800

    更新数据

--- src/components/Assets.vue
+++ src/components/Assets.vue
@@ -10,10 +10,10 @@
         <a-asset-item id="avatarModel"
             src="https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.2.0/examples/image-tracking/assets/card-example/softmind/scene.gltf"></a-asset-item> -->
 
-        <img id="bus_station" src="/src/assets/image/placeIcon/bus-line.png" />
-        <img id="hospital" src="/src/assets/image/placeIcon/stethoscope-fill.png" />
-        <img id="shopping_mall" src="/src/assets/image/placeIcon/shopping-basket-2-fill.png" />
-        <img id="restaurant" src="/src/assets/image/placeIcon/restaurant-line.png" />
-        <img id="fire_station" src="/src/assets/image/placeIcon/shield-check-line.png" />
+        <img id="bus_station" src="/images/placeIcon/bus-line.png" />
+        <img id="hospital" src="/images/placeIcon/stethoscope-fill.png" />
+        <img id="shopping_mall" src="/images/placeIcon/shopping-basket-2-fill.png" />
+        <img id="restaurant" src="/images/placeIcon/restaurant-line.png" />
+        <img id="fire_station" src="/images/placeIcon/shield-check-line.png" />
     </a-assets>
 </template>
